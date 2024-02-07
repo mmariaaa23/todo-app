474b54a57477156e2f1eee98aa399d360f2182ed
Maria <mmariaaa23@gmail.com>
2024-02-07 15:35:10 +0300

    верстка sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import List from './components/List';
+import listSvg from './assets/img/list.svg';
+function App ()
+{
+	return (
+    <div className = 'todo'>
+            <div className="todo__sidebar">
+                <List items={[
+                    {
+                        icon: (listSvg),
+                        name: 'Все задачи',
+                        active: true,
+                    }
+                ] } />
+                <List items={[
+                    {
+                        color: 'green',
+                        name: 'Покупки',
+                    },
+                    {
+                        color: 'light-blue',
+                        name: 'Фронтенд',
+                    },
+                    {
+                        color: 'pink',
+                        name: 'Фильмы и сериалы',
+                    }
+                ] } />
+            </div>
+            <div className="todo__tasks"></div>
     </div>
-  );
+	);
 }
 
 export default App;
